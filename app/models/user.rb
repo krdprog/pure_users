826670c2f039c92339e293fc7c3ee4bde5e725a0
Tsaplin-Kupaysinov <info@krdprog.ru>
2019-12-27 10:40:59 +0300

    Fixed email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
+
   mount_uploader :avatar, AvatarUploader
 
   validates :name, :email, :avatar, presence: true
-  validates :email, uniqueness: true
+  validates :email, uniqueness: { case_sensitive: true }
 end
