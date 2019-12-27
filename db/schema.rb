c5f128fe7e9195185359641a4efd70965cc017dd
Tsaplin-Kupaysinov <info@krdprog.ru>
2019-12-27 10:54:59 +0300

    Added migration to User with index for uniqueness emails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_25_162015) do
+ActiveRecord::Schema.define(version: 2019_12_27_074454) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_12_25_162015) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "avatar"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
