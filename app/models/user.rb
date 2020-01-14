f51811caf6e5b66a261e651cb26a7ec8fd429b1c
Tsaplin-Kupaysinov <info@krdprog.ru>
2020-01-14 11:15:36 +0300

    Added validate: true password length in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   validates :name, :email, :avatar, :password_digest, presence: true
   validates :email, uniqueness: { case_sensitive: true }
   validates :email, format: { with: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/, message: "Неправильный формат e-mail адреса" }
+  validates :password, length: { minimum: 6, maximum: 72, message: "Неправильная длина пароля. Правильно: от 6 до 72 знаков." }
 
   has_secure_password
 end
