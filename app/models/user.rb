bb1705cf1aa3010dacc733ec9135f665f609c5d9
Tsaplin-Kupaysinov <info@krdprog.ru>
2020-01-14 11:11:56 +0300

    Added validate: true email format in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
 
   validates :name, :email, :avatar, :password_digest, presence: true
   validates :email, uniqueness: { case_sensitive: true }
+  validates :email, format: { with: /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/, message: "Неправильный формат e-mail адреса" }
 
   has_secure_password
 end
